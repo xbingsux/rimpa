ef6dd6d77dd54168c08c4facfc9178351f4d36e2
aonAaon <abotaon01@gmail.com>
2025-01-28 17:56:17 +0700

    Create app-color.constatnt

---
+++ mobile/lib/core/constant/app-colors.constant.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgrondLight = Color(0xFFFFFFFF);
+  static const Color backgroundDark = Color(0xFF212529);
+
+  static const Color textPrimary = Color(0xFF212529);
+  static const Color textSecondary = Color(0xFF6C757D);
+  static const Color textDanger = Color(0xFFDC3545);
+  static const Color textWarning = Color(0xFFFFC107);
+  static const Color textSuccess = Color(0xFF198754);
+  static const Color textInfo = Color(0xFF0DCAF0);
+  static const Color textLight = Color(0xFFF8F9FA);
+}
