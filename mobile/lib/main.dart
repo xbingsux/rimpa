2fb1c70d72802bc3562edc5ce722e1fb0c00ca7e
warawutkanya <warawutkanya@gmail.com>
2025-01-29 14:44:48 +0700

    test change main

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final themeController = Get.find<ThemeController>();
     return Obx(() => GetMaterialApp(
-          title: 'Login rimpaa',
+          title: 'Login rimpaad',
           theme: AppTheme.lightTheme,
           darkTheme: AppTheme.darkTheme,
           themeMode:
