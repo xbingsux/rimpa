b36415e79118a389ed19fb932f8f423b65da8e9d
LnwSanghan <werapat065243@gmail.com>
2025-02-07 13:04:07 +0700

    home chang_profile

--- mobile/lib/modules/views/home/home_profile.dart
+++ mobile/lib/modules/views/home/home_profile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../../controllers/getusercontroller/auth_service.dart';  // นำเข้า AuthService ที่คุณสร้างไว้
+import '../../controllers/getusercontroller/auth_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../../core/constant/app.constant.dart';
 
@@ -9,102 +9,193 @@ class HomeProfilePage extends StatefulWidget {
   _HomeProfilePageState createState() => _HomeProfilePageState();
 }
 
-class _HomeProfilePageState extends State<HomeProfilePage> {
-  String email = ''; // ตัวแปรสำหรับเก็บ email
-  bool isLoggedIn = false; // ตัวแปรตรวจสอบสถานะล็อกอิน
+class _HomeProfilePageState extends State<HomeProfilePage>
+    with SingleTickerProviderStateMixin {
+  String email = '';
+  bool isLoggedIn = false;
+  late AnimationController _animationController;
+  late Animation<Offset> _slideAnimation;
 
   @override
   void initState() {
     super.initState();
-    _checkLoginStatus(); // ตรวจสอบสถานะล็อกอินเมื่อหน้าเปิด
+    _checkLoginStatus();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+
+    _slideAnimation = Tween<Offset>(begin: Offset(0, 1), end: Offset(0, 0))
+        .animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+
+    _animationController.forward();
   }
 
   void _checkLoginStatus() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? token = prefs.getString('token'); // ตรวจสอบค่า token
+    String? token = prefs.getString('token');
     if (token != null && token.isNotEmpty) {
-      // ถ้ามี token ก็ให้โหลดข้อมูลผู้ใช้
       _loadUserInfo();
     } else {
-      // ถ้าไม่มี token แสดงว่าไม่ได้ล็อกอิน
       setState(() {
-        isLoggedIn = false; // ตั้งสถานะเป็นยังไม่ได้ล็อกอิน
+        isLoggedIn = false;
       });
     }
   }
 
   void _loadUserInfo() async {
     final authService = AuthService();
-    Map<String, String> userInfo = await authService.getUserInfo(); // ดึงข้อมูลผู้ใช้
-    print("User Info: $userInfo"); // ตรวจสอบค่าที่ได้รับจาก SharedPreferences
+    Map<String, String> userInfo = await authService.getUserInfo();
     setState(() {
-      email = userInfo['email'] ?? 'ไม่มีข้อมูล'; // ถ้าไม่ได้รับข้อมูลจะให้ข้อความนี้แทน
-      isLoggedIn = true; // ตั้งสถานะเป็นล็อกอินแล้ว
+      email = userInfo['email'] ?? 'ไม่มีข้อมูล';
+      isLoggedIn = true;
     });
   }
 
   void _logout() async {
     final authService = AuthService();
-    await authService.clearAuth(); // ลบข้อมูลล็อกอิน
+    await authService.clearAuth();
     setState(() {
-      isLoggedIn = false; // ตั้งสถานะเป็นไม่ได้ล็อกอิน
-      email = ''; // เคลียร์ email
+      isLoggedIn = false;
+      email = '';
     });
   }
 
- @override
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        gradient: AppGradiant.gradientX_1, // ใช้ Gradient พื้นหลัง
+        gradient: AppGradiant.gradientX_1,
       ),
       child: SafeArea(
         child: Scaffold(
-          backgroundColor: Colors.transparent, // พื้นหลังใส
-          body: Center(
-            child: isLoggedIn
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        email.isEmpty ? "กำลังโหลด..." : email,
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
+          backgroundColor: Colors.transparent,
+          body: SlideTransition(
+            position: _slideAnimation,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  // BottomSheet with adjusted position
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.75,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).brightness == Brightness.dark
+                          ? Color.fromARGB(255, 26, 25, 25)
+                          : Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
                       ),
-                      SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: _logout,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.redAccent,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 10,
+                          spreadRadius: 2,
                         ),
-                        child: Text('ออกจากระบบ'),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 60), // No padding at the top
+                      child: Center(
+                        child: isLoggedIn
+                            ? Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.start, // Move to top
+                                children: [
+                                  // User name (Bold and not too big)
+                                  Text(
+                                    "ชื่อผู้ใช้", // เปลี่ยนเป็นชื่อผู้ใช้จริงได้
+                                    style: TextStyle(
+                                      fontSize: AppTextSize
+                                          .sm, // ขนาดตัวอักษรไม่ใหญ่เกินไป
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                  // Email
+                                  Text(
+                                    email.isEmpty ? "กำลังโหลด..." : email,
+                                    style: TextStyle(
+                                      fontSize:
+                                          AppTextSize.md, // ขนาดตัวอักษรเล็กลง
+                                    ),
+                                  ),
+                                  SizedBox(height: 20),
+
+                                  // Logout button
+                                  ElevatedButton(
+                                    onPressed: _logout,
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.redAccent,
+                                      minimumSize: Size(double.infinity, 50),
+                                    ),
+                                    child: Text('ออกจากระบบ'),
+                                  ),
+                                ],
+                              )
+                            : Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.start, // Move to top
+                                children: [
+                                  Text(
+                                    "ยังไม่ได้ล็อกอิน",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  SizedBox(height: 20),
+                                  ElevatedButton(
+                                    onPressed: () => Get.toNamed('/login'),
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.blueAccent,
+                                      minimumSize: Size(double.infinity, 50),
+                                    ),
+                                    child: Text('ไปยังหน้าล็อกอิน'),
+                                  ),
+                                ],
+                              ),
                       ),
-                    ],
-                  )
-                : Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "ยังไม่ได้ล็อกอิน",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: () => Get.toNamed('/login'),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blueAccent,
-                        ),
-                        child: Text('ไปยังหน้าล็อกอิน'),
-                      ),
-                    ],
+                    ),
                   ),
+
+                  // Profile Circle Positioned at the top
+                  Positioned(
+                    top: -50,
+                    left: MediaQuery.of(context).size.width / 2 - 50,
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.black,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 10,
+                            spreadRadius: 5,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
