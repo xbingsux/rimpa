aa7a1abbbce7e4046cee14df0ad9841ce30df54d
Puttipong <puttipong.chomchit@gmail.com>
2025-02-25 15:16:33 +0700

    create admin

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,6 +23,19 @@ app.use("/", require("./controllers/upload/controller"));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 //start server
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 app.listen(port, () => {
+  prisma.role.upsert({
+    where: {
+      role_name: 'admin'
+    },
+    create: {
+      role_name: 'admin'
+    }, update: {
+
+    }
+  })
   console.log(`Listening on port ${port}`);
 });
