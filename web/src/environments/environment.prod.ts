96782e255ed31225513d9e1464435e108978af71
PuttipongChomchit <puttipong.chomchit@gmail.com>
2025-03-11 19:21:03 +0000

    .

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   SECRET_KEY: 'test',
-  API_URL: 'http://localhost:3001',
+  // API_URL: 'http://localhost:3001',
+  API_URL: 'https://api-rimpa.nightbears.co',
   timeZone: 'Asia/Bangkok'
 };
--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   SECRET_KEY: 'test',
-  // API_URL: 'https://api-rimpa.nightbears.co',
-  API_URL: 'http://localhost:3001',
+  API_URL: 'https://api-rimpa.nightbears.co',
+  // API_URL: 'http://localhost:3001',
   timeZone: 'UTC+7'
 };
