bf75ec5f81c3770e8a826393a16e3c7721238427
Puttipong <puttipong.chomchit@gmail.com>
2025-03-12 14:42:10 +0700

    update gitignore

--- web/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  SECRET_KEY: 'test',
-  // API_URL: 'http://localhost:3001',
-  API_URL: 'https://api-rimpa.nightbears.co',
-  timeZone: 'Asia/Bangkok'
-};
--- web/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  SECRET_KEY: 'test',
-  API_URL: 'https://api-rimpa.nightbears.co',
-  // API_URL: 'http://localhost:3001',
-  timeZone: 'UTC+7'
-};
