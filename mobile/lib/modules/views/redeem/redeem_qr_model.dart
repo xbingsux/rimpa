08384dd3029f1bcd75cbe6af5e3795e61adfd86d
ananyalux.dev <ananyalux.dev@myorder.ai>
2025-03-27 16:03:13 +0700

    add redeem barcode

--- mobile/lib/modules/views/redeem/redeem_qr_model.dart
+++ mobile/lib/modules/views/redeem/redeem_qr_model.dart
@@ -1,6 +1,7 @@
 class RedeemQRModel {
   final String status;
   final String token;
+  final String barcode;
   final DateTime issuedAt;
   final DateTime expiresAt;
 
@@ -9,12 +10,14 @@ class RedeemQRModel {
     required this.token,
     required this.issuedAt,
     required this.expiresAt,
+    required this.barcode,
   });
 
   factory RedeemQRModel.fromJson(Map<String, dynamic> json) {
     return RedeemQRModel(
       status: json['status'],
       token: json['token'],
+      barcode: json['barcode'],
       issuedAt: DateTime.fromMillisecondsSinceEpoch(json['iat'] * 1000),
       expiresAt: DateTime.fromMillisecondsSinceEpoch(json['exp'] * 1000),
     );
